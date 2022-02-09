efd9a0a927197124fe9a8fa39274b771ba40a913
Joe Ruggieri <joe@bulb.co.uk>
2022-02-09 13:18:33 +0000

    dohdle 1

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        'simpsons-yellow': '#fed90f',
+        'simpsons-dark': '#424F46',
+      },
+    },
+  },
+  plugins: [],
+}
